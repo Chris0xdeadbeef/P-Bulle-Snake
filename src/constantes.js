ea0694648879c7b0ff31b69ff3b7e403a07c01f1
chrisETML <christopher.ristic@yahoo.fr>
2025-09-08 09:14:21 +0200

    refactor(constantes):export modifié

--- src/constantes.js
+++ src/constantes.js
@@ -1,16 +1,18 @@
-  export const Direction = {
+const Direction = {
     UP: "UP",
     DOWN: "DOWN",
     LEFT: "LEFT",
     RIGHT: "RIGHT"
-  };
+};
 
-  export const ArrowKey = {
+const ArrowKey = {
     UP: "ArrowUp",
     DOWN: "ArrowDown",
     LEFT: "ArrowLeft",
     RIGHT: "ArrowRight"
-  };
+};
 
-  export const snakeHeadColor = "green";
-  export const snakeBodyColor = "lightgreen";
+const snakeHeadColor = "green";
+const snakeBodyColor = "lightgreen";
+
+export { snakeHeadColor, snakeBodyColor, Direction, ArrowKey };
