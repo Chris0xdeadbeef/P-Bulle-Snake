f60e2fb85371fbd19f656b216206972466b31c65
chrisETML <christopher.ristic@yahoo.fr>
2025-09-08 09:03:51 +0200

    chore(constantes.js): extraction des constantes dans un fichier

---
+++ src/constantes.js
@@ -0,0 +1,15 @@
+  export const Direction = {
+    UP: "UP",
+    DOWN: "DOWN",
+    LEFT: "LEFT",
+    RIGHT: "RIGHT"
+  };
+
+  export const ArrowKey = {
+    UP: "ArrowUp",
+    DOWN: "ArrowDown",
+    LEFT: "ArrowLeft",
+    RIGHT: "ArrowRight"
+  };
+
+  export const snakeHeadColor = "green";
