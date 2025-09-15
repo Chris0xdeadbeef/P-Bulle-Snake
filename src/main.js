0f10b2074ab7a68f1bbac5de0cec536cc3ef61f2
Christopher Ristic <pu36ylg@eduvaud.ch>
2025-09-15 09:26:33 +0200

    feat(main): appel des collisions

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import { generateFood, drawFood } from "./food.js";
 import { handleDirectionChange } from "./controls.js";
 import { checkCollision, checkWallCollision } from "./collision.js";
 import { drawScore } from "./score.js";
-import {direction as Direction} from "./constantes.js";
+import {direction as Direction, LAYERS} from "./constantes.js";
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
@@ -27,6 +27,7 @@ let food;
 let direction = Direction.RIGHT; // Direction initiale
 let score = 0;
 let gameInterval; // Variable pour stocker l'identifiant de l'intervalle
+let entities = [];// Liste des entités du jeu (murs internes, bonus, etc.)
 
 document.addEventListener("keydown", (event) => {
   direction = handleDirectionChange(event, direction);
@@ -43,10 +44,20 @@ function draw() {
 
   //Efface tout le canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const head = snake[0];
 
   drawScore(ctx, score, 10, 30);
   drawSnake(ctx, snake, box);
   moveSnake(snake, direction, box);
+  
+   // Vérifie collisions et game over
+  if (checkCollision(head, snake) || checkWallCollision(head, canvas, box, entities)) {
+    clearInterval(gameInterval);
+    alert("Game Over !");
+    return;
+  }
+
+  
 }
 
 startGame();
